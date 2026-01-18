c93b6adbdc7d17097ee2b762426b250c113633e9
trianajuand <trianajuandiego@gmail.com>
2026-01-18 16:10:59 -0500

    D:/Git/

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import RedirectResponse
 import tempfile
 import os
 
@@ -9,6 +10,11 @@ app = FastAPI()
 
 def get_db():
     return DatabaseHandler()
+
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
 
 
 @app.post("/upload-audio")
